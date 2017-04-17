89dd5c07e865906ffd5730e09a9e91b30c31713e
Samuel Breck <breck@usc.edu>
2017-04-17 12:44:20 -0700

    deploy attempt 1

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(bodyParser.json());
 
 // app.set('port', (3001));
 
+var path = require('path');
 app.set('port', (process.env.PORT || 3001));
 
 var mongoose = require('mongoose');
@@ -43,7 +44,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', function (req, res){
-    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'Front-End/build', 'index.html'));
   });
 }
 
