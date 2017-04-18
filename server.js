1dcd70340d3a655bf502f2fe6eb029fb6c53c1ce
Samuel Breck <breck@usc.edu>
2017-04-17 21:35:55 -0700

    deploy for the love of jesus

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ var api = require('./routes/api');
 app.use('/api', api);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('Front-End/build'));
 
   app.get('*', function (req, res){
     res.sendFile(path.resolve(__dirname, 'Front-End/build', 'index.html'));
